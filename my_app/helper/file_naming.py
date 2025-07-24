1a2ef28c9af05c3ee4d57cd7fa35a0f5eab86be4
CosmoWorker <social.tarang@gmail.com>
2025-07-24 20:02:35 +0530

    remove check with extracting basename

--- my_app/helper/file_naming.py
+++ my_app/helper/file_naming.py
@@ -1,13 +1,20 @@
 import frappe, os, shutil
 
-def file_retitling(original_filename: str, folder: str, title: str) -> dict:
+def file_retitling(original_file_url: str, folder: str, title: str) -> dict:
     
     '''Renaming uploaded video filename with the generated title for that video_info record and returns info in JSON- dict '''
-    original_path=frappe.get_site_path("public", folder, original_filename)
+    
+    original_filename=original_file_url.replace("/files", "").lstrip("/")
+    default_path = frappe.get_site_path("public","files", original_filename)
+
+    # if not os.path.exists(default_path):
+    #     frappe.throw(f"Uploaded file {original_filename} not found in public/files/")
+
     base, ext=os.path.splitext(original_filename)
-    new_filename=f"{base}-{title}{ext}"
+    new_base=f'{base}_{title.replace(" ", "-")}'
+    new_filename=f'{new_base}{ext}'
     new_path=os.path.join(frappe.get_site_path("public", folder), new_filename)
-    shutil.move(original_path, new_path)
+    shutil.move(default_path, new_path)
 
     file_info={
         "original_filename": original_filename,
@@ -15,7 +22,8 @@ def file_retitling(original_filename: str, folder: str, title: str) -> dict:
         "basename": base,
         "extension": ext,
         "new_filepath": new_path,
-        "new_filename": new_filename
+        "new_filename": new_filename,
+        "new_basename": new_base
     }
 
     return file_info
