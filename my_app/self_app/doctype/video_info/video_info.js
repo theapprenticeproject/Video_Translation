6f3b77c94f34627532796e35e2bbcf08d4d68c90
CosmoWorker <social.tarang@gmail.com>
2025-07-25 23:06:37 +0530

    updated approach with processing after upload

--- my_app/self_app/doctype/video_info/video_info.js
+++ my_app/self_app/doctype/video_info/video_info.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2025, VT and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Video Info", {
-// 	refresh(frm) {
+frappe.ui.form.on("Video Info", {
 
-// 	},
-// });
+    original_vid: (frm)=>{
+        if (frm.doc.original_vid){
+            frappe.show_alert("Video Uploaded", 4)
+        }
+    },
+
+    onload: (frm)=>{
+        frappe.realtime.on("audio_extraction_completed", (data)=>{
+            console.log(data)
+            if (data.videopath_url === frm.doc.original_vid){
+                const audiopath=data.audiofile_url
+                frm.set_value("original_audio_extracted", audiopath)
+                .then(()=>{
+                    frappe.show_alert({"message": __("Audio Extracted"), indicator: "green"}, 3)
+                    console.log(audiopath, "audiopath after realtime event published")
+                    frm.save()
+                    console.log("after save")
+                })
+                .catch((e)=>{
+                    console.log("Error setting audiopath", e)
+                    frm.save()
+                })
+            }else{
+                console.log("Realtime videopath_url mismatch", data.videopath_url, "vs", frm.doc.original_vid)
+            }
+        })
+    },
+});
