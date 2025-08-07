790bfcd2dcf217715e1d87383c29446672abf1db
CosmoWorker <social.tarang@gmail.com>
2025-08-07 23:55:25 +0530

    removed sample unnecessary doctype article

--- my_app/self_app/doctype/article/article.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, VT and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Article",    {
-    refresh: function(frm){
-        msgprint("Hello, tnx for creating an article")
-    }
-});
