54b0adfdf31fd88cce98d22b1982a27397592fa7
CosmoWorker <social.tarang@gmail.com>
2025-08-29 15:31:53 +0530

    feat: added video_preview field & client side script for loading video and subs altogether

--- my_app/api/v1/subtitle.py
+++ my_app/api/v1/subtitle.py
@@ -1,17 +1,14 @@
-import frappe, subprocess, math
+import frappe, math, os
 from groq import Groq
 
 client=Groq(
     api_key=frappe.conf.groq_api_key
 )
 
-def srt_generate(video_filename: str, audio_filename: str, langCode: str, processed_docname: str):
+def vtt_generate(audio_filename: str, processed_docname: str):
     processed_doc=frappe.get_doc("Processed Video Info", processed_docname)
     audio_file_path=frappe.get_site_path("public", "files", "processed", audio_filename)
-    subtitled_video=frappe.get_site_path("public", "files", "processed",f"sub_{video_filename}")
-    translatedVid=frappe.get_site_path("public", "files", "processed",video_filename)
-
-    # formatting time in HH:MM:SS,mmm ; 'm' denoting milliseconds
+    # formatting time in HH:MM:SS.mmm ; 'm' denoting milliseconds
     def format_time(seconds):
         hours=math.floor(seconds/3600)
         seconds%=3600
@@ -19,50 +16,28 @@ def srt_generate(video_filename: str, audio_filename: str, langCode: str, proces
         seconds%=60
         milliseconds=math.floor((seconds - math.floor(seconds))*1000)   
         seconds=math.floor(seconds)
-        formatted_time=f'{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}'
+        formatted_time=f'{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}'
 
         return formatted_time
 
 
-    def generate_srt_file(segments):
-        transcript_timestamps=frappe.get_site_path("public", "files", "processed","subtitles_timestamps.srt")
-        with open(transcript_timestamps, "w") as f:
-            text=""
-            for idx, segment in enumerate(segments):
+    def generate_vtt_file(segments, audio_filename):
+        timestamps_path=frappe.get_site_path("public", "files", "processed", f'{audio_filename.replace("wav", "vtt")}')
+        with open(timestamps_path, "w") as f:
+            text="WEBVTT\n\n"   
+            for segment in segments:
                 segment_start=format_time(segment["start"])
                 segment_end=format_time(segment["end"])
-                text+=f'{str(idx+1)}\n'
                 text+=f'{segment_start} --> {segment_end}\n'
-                text+=f'{segment["text"].strip()}\n'
-                text+='\n'
+                text+=f'{segment["text"].strip()}\n\n'
             f.write(text)
-        add_subtitle_vid(translatedVid, transcript_timestamps, subtitled_video)
 
-    with open(audio_file_path, "rb") as file:
-        transcription = client.audio.transcriptions.create(
-            file = file,
-            model = "whisper-large-v3",
-            response_format = "verbose_json",
-            language = langCode,
-            timestamp_granularities = ["segment"] 
-        )
-        generate_srt_file(transcription.segments)
-
-    def add_subtitle_vid(input_vid, subtitle_file, output_video):
         try:
-            # adding 'soft subtitle' to the original video
-            subprocess.call(["ffmpeg", "-i", input_vid, "-i", subtitle_file, "-c", "copy", "-c:s", "mov_text", output_video])
-
+            # subprocess.call(["ffmpeg", "-i", input_vid, "-i", subtitle_file, "-c", "copy", "-c:s", "mov_text", output_video])
+            processed_doc.translated_subs=f'files/processed/{os.path.basename(timestamps_path)}'
             processed_doc.status="Subtitle added to translated video"
             processed_doc.save(ignore_permissions=True)
             frappe.db.commit()
-                
-        except subprocess.SubprocessError as e:
-            processed_doc.status="Subtitle Generation Failed - subprocess"
-            processed_doc.save(ignore_permissions=True)
-            frappe.db.commit()
-
-            frappe.throw('ffmpeg- Video Subtitling error : ', e)
 
         except Exception as err:
             processed_doc.status="Subtitle Generation Failed - exception"
@@ -70,5 +45,15 @@ def srt_generate(video_filename: str, audio_filename: str, langCode: str, proces
             frappe.db.commit()
             
             frappe.throw("Error during Subtitling generation : ", err)            
+
+    with open(audio_file_path, "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            file = file,
+            model = "whisper-large-v3-turbo",
+            response_format = "verbose_json",
+            # language = langCode, ( optional )
+            timestamp_granularities = ["segment"] 
+        )
+        generate_vtt_file(transcription.segments, audio_filename)
 
  
--- my_app/self_app/doctype/processed_video_info/processed_video_info.js
+++ my_app/self_app/doctype/processed_video_info/processed_video_info.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2025, VT and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Processed Video Info", {
-// 	refresh(frm) {
+frappe.ui.form.on("Processed Video Info", {
+	refresh(frm) {
+        if (frm.doc.localized_vid){
+            const videoPath=frm.doc.localized_vid
+            const subtitlePath=frm.doc.translated_subs
 
-// 	},
-// });
+            frm.fields_dict.video_preview.$wrapper.html(`
+                <video controls width="640" height="360">
+                    <source src="${videoPath}" type="video/mp4">
+                    <track src="${subtitlePath}" kind="subtitles" default>
+                </video>    
+            `)
+        }else{
+            frm.fields_dict.video_preview.$wrapper.empty()
+        }
+	},
+});
