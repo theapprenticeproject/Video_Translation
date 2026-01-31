8772136b8ca8b9eb988d7d060abe4a04eb9ea0bf
CosmoWorker <social.tarang@gmail.com>
2026-01-31 23:41:13 +0530

    feat: added initial dialog for retry options

--- my_app/self_app/doctype/processed_video_info/processed_video_info.js
+++ my_app/self_app/doctype/processed_video_info/processed_video_info.js
@@ -20,29 +20,47 @@ frappe.ui.form.on("Processed Video Info", {
         frm.clear_custom_buttons();
         if ((frm.doc.status || "").trim() !== "pending") {
             frm.add_custom_button("Retry", () => {
-                frappe.show_alert("Re-processing...", "orange")
-                frappe.db.get_value("Video Info", frm.doc.origin_vid_link, "target_lang").then(
-                    r => {
-                        const tar_lang = r.message.target_lang
-                        frappe.call({
-                            method: "my_app.media-queues.tasks_pipe.retry_trigger",
-                            args: {
-                                video_info_name: frm.doc.origin_vid_link,
-                                tar_lang: tar_lang,
-                                processed_docname: frm.doc.name,
+
+                const d = new frappe.ui.Dialog({
+                    title: "Retry Options",
+                    fields: [
+                        {
+                            label: "Key Terms",
+                            fieldname: "keyterm_prompt",
+                            description: "Comma-separated words or phrases for correct recognition during transcription",
+                            fieldtype: "Data"
+                        },
+                        {
+                            fieldname: "pronunciation_dict",
+                            label: "Pronounciation Dictionary",
+                            description: "One rule per line (word-pronunciation). Ex: शुक्र-सुक्कुर, Apprentice-uh-pren-tis",
+                            fieldtype: "Small Text"
+                        }
+                    ],
+                    primary_action_label: "Retry",
+                    primary_action(values) {
+                        frappe.show_alert("Re-processing...", "orange")
+                        frappe.db.get_value("Video Info", frm.doc.origin_vid_link, "target_lang").then(
+                            r => {
+                                const tar_lang = r.message.target_lang
+                                frappe.call({
+                                    method: "my_app.media-queues.tasks_pipe.retry_trigger",
+                                    args: {
+                                        video_info_name: frm.doc.origin_vid_link,
+                                        tar_lang: tar_lang,
+                                        processed_docname: frm.doc.name,
+                                        options: values
+                                    }
+                                })
+                                console.log("target language: ", tar_lang)
                             }
-                        })
-                        console.log("target language: ", tar_lang)
+                        )
+                        d.hide();
                     }
-                )
+                })
+                d.show();
+
             })
         }
     },
 });
-
-
-const all_fields_filled = (frm) => {
-    const required_fields = ["localized_vid", "translated_aud", "processed_on", "status", "origin_vid_link", "translated_subs"]
-
-    return required_fields.every((field) => !!frm.doc[field])
-}
